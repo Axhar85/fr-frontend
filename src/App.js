1b7cd5b7178ac1eebd8a0c31fbfcffad6fd1d3a5
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-17 14:25:06 +0200

    creating a connection store data from front end to back end

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  let [data, setData]= useState([])
+  let [user, setUser]= useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:5000/students')
+      .then(response => response.json())
+      .then(data => setData(data))
+  }, [])
+
+  const handleForm = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:5000/students', {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({
+        user: user
+      })
+    })
+  }
+
   return (
     <div className="App">
-    <h1>Students</h1>
-      <form>
-        <input placeholder= 'Student name ...' />
+    <h1>Wild Students</h1>
+      <form onSubmit= {handleForm}>
+        <input 
+          value={user}
+          onChange={(event) => setUser(event.target.value)}
+          placeholder= 'Student name ...' />
         <button>Add student</button>
       </form>
       <h3>List of students</h3>
+      {
+        data.map((student, index) => (
+          <p key={index}>{student.user}</p>
+        ))
+      }
     </div>
   );
 }
